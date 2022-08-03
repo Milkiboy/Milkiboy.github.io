53d46028bc2b37d1e533915e5346e4a0f208aada
Milkias Mebrahru <milkiboy2016@gmail.com>
2022-08-03 00:05:37 -0500

    update

--- SimpleCalculaor/addmod.js
+++ SimpleCalculaor/addmod.js
@@ -1,69 +1,32 @@
 
-
-    exports.add = function (req,res,vals) {
-        var sum = parseInt(vals.first) + parseInt(vals.second);
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write("<!DOCTYPE html>");
-        res.write("<html>");
-        res.write("<head><meta charset=\"utf-8\"/>");
-        res.write("<title>Calculator Web Site</title>");
-        res.write("</head>");
-        res.write("<body>");
-        res.write("<p>The Answer is: ");
-        res.write(String(sum));
-        res.write("</p>");
-        res.write("</body>");
-        res.write("</html>");
-        return res.end();
-        };
-
-        exports.miinus = function (req,res,vals) {
-            var dif = parseInt(vals.first) - parseInt(vals.second);
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write("<!DOCTYPE html>");
-            res.write("<html>");
-            res.write("<head><meta charset=\"utf-8\"/>");
-            res.write("<title>Calculator Web Site</title>");
-            res.write("</head>");
-            res.write("<body>");
-            res.write("<p>The Answer is: ");
-            res.write(String(dif));
-            res.write("</p>");
-            res.write("</body>");
-            res.write("</html>");
-            return res.end();
-            };
-
-            exports.add = function (req,res,vals) {
-                var mul = parseInt(vals.first) * parseInt(vals.second);
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write("<!DOCTYPE html>");
-                res.write("<html>");
-                res.write("<head><meta charset=\"utf-8\"/>");
-                res.write("<title>Calculator Web Site</title>");
-                res.write("</head>");
-                res.write("<body>");
-                res.write("<p>The Answer is: ");
-                res.write(String(mul));
-                res.write("</p>");
-                res.write("</body>");
-                res.write("</html>");
-                return res.end();
-                };
-
-                exports.add = function (req,res,vals) {
-                    var divv = parseInt(vals.first) / parseInt(vals.second);
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.write("<!DOCTYPE html>");
-                    res.write("<html>");
-                    res.write("<head><meta charset=\"utf-8\"/>");
-                    res.write("<title>Calculator Web Site</title>");
-                    res.write("</head>");
-                    res.write("<body>");
-                    res.write("<p>The Answer is: ");
-                    res.write(String(divv));
-                    res.write("</p>");
-                    res.write("</body>");
-                    res.write("</html>");
-                    return res.end();
-                    };
+var result=0;
+exports.add = function (req,res,vals) {
+    result=parseInt(vals.first)+parseInt(vals.second)
+};
+exports.subtract = function (req,res,vals) {
+    result=parseInt(vals.first)-parseInt(vals.second)
+};
+exports.multiply = function (req,res,vals) {
+    result=parseInt(vals.first)*parseInt(vals.second)
+};
+exports.division = function (req,res,vals) {
+    result=(parseInt(vals.first))/(parseInt(vals.second))
+};
+exports.displays=function(req,res,vals){
+    res.writeHead(200, {'Content-Type': 'text/html'}); 
+    res.write("<!DOCTYPE html>");
+    res.write("<html>");
+    res.write("<head><meta charset=\"utf-8\"/>"); 
+    res.write("<title>Calculator Web Site</title>"); 
+    res.write("</head>");
+    res.write("<body>");
+    res.write("<p style=\"background-color:orange\" ; style=\"font:14pt bold\";>The result is: ");
+          res.write(String(result));
+    res.write("</p>");
+    res.write(
+        "<a href='https://amannegash.github.io/myFirstRepository.github.io/Simple%20Calculator%20Lab/'>Another calculation</a>"
+      );
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+};
